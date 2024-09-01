e4b4a2fd52ac5fb068089077e923ba53e0763a09
RadmanRAZ <radmana92@gmail.com>
2024-09-01 17:33:55 +0330

    ui of search part1

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
+
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='text-white-800 flex-between text-sm w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col'>
+      <p>Copyright all right reserverd</p>
+      <div className='flex gap-x-9'>
+        <Link href='/terms-of-use' >Terms and conditions</Link>
+        <Link href='/privacy-policy' >privacy policy</Link>
+      </div>
+    </footer>
   )
 }
 
