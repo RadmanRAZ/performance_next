9636987442100738428a809a8c8257a8b61f4983
RadmanRAZ <radmana92@gmail.com>
2024-09-08 23:24:20 +0330

    fixed issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,14 +11,14 @@ const Header = ({title , query , category} : Props) => {
 
    return (
     <h1 className='heading3 self-start text-white-800' >
-      Search results for "{query}" in <span className='capitalize'>{category}</span>
+      Search results for {query} in <span className='capitalize'>{category}</span>
     </h1>
    ) 
 
    if(query){
     return(
       <h1 className='heading3 self-start text-white-800' >
-      Search results for "{query}" 
+      Search results for {query}
     </h1>
     )
    }
@@ -26,7 +26,7 @@ const Header = ({title , query , category} : Props) => {
    if(category){
     return(
       <h1 className='heading3 self-start text-white-800' >
-      Search results for "{category}" 
+      Search results for {category}
     </h1>
     )
    }
